a983dfc2cf9b3b63ec2c4345cf1a6f7fa13a9dc2
Khanh <daugauvuontre@gmail.com>
2020-10-30 23:24:50 +0700

    Model validation

--- basic/basic_data_exploration/basic.py
+++ basic/basic_data_exploration/basic.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
@@ -20,10 +22,18 @@ print(X.describe())
 print(X.head())
 
 melbourne_model = DecisionTreeRegressor(random_state=1)
-melbourne_model.fit(X, y)
 
-print('Predict for just %s:' % len(X.head()))
-print(X.head())
-predict = melbourne_model.predict(X.head())
-print('Predictions are:')
-print(predict)
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+melbourne_model.fit(train_X, train_y)
+
+# print('Predict for just %s:' % len(X.head()))
+# print(X.head())
+# predict = melbourne_model.predict(X.head())
+# print('Predictions are:')
+# print(predict)
+
+predict = melbourne_model.predict(val_X)
+
+mae = mean_absolute_error(val_y, predict)
+print('Mean Absolute Error')
+print(mae)

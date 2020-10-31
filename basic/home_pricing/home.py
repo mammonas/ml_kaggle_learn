ebac70dc8fe6b4b8fb74ac6bc60857f3a8f8c067
Khanh <daugauvuontre@gmail.com>
2020-10-31 22:50:08 +0700

    We have an improvement

--- basic/home_pricing/home.py
+++ basic/home_pricing/home.py
@@ -1,34 +1,36 @@
 import pandas as pd
-import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 train_file_path = './home-data-for-ml-course/train.csv'
 train_data = pd.read_csv(train_file_path)
-train_data = pd.get_dummies(train_data)
-
-train_features = ['MSSubClass', 'LotFrontage', 'LotArea', 'YearBuilt', '1stFlrSF', 'FullBath', 'BedroomAbvGr',
+features = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', '1stFlrSF', 'FullBath', 'BedroomAbvGr',
             'KitchenAbvGr', 'GarageArea']
-train_X = train_data[train_features]
-train_X = np.nan_to_num(train_X)
-train_y = np.nan_to_num(train_data.SalePrice)
-# train_y = train_data.SalePrice
+
+train_X = train_data[features]
+train_X = train_X.fillna(train_X.mean())
+train_y = train_data.SalePrice
+
+s_train_X, s_val_X, s_train_y, s_val_y = train_test_split(train_X, train_y, random_state=0)
+print('Train count: %i %i' % (len(s_train_X), len(s_train_y)))
+print('Test count: %i %i' % (len(s_val_X), len(s_val_y)))
 rf_model = RandomForestRegressor(random_state=1)
-rf_model.fit(train_X, train_y)
+rf_model.fit(s_train_X, s_train_y)
+
+predicts = rf_model.predict(s_val_X)
+mae = mean_absolute_error(predicts, s_val_y)
+print('MAE %i' % mae)
+
 
 test_file_path = './home-data-for-ml-course/test.csv'
 test_data = pd.read_csv(test_file_path)
-test_data = pd.get_dummies(test_data)
-test_features = ['MSSubClass', 'LotFrontage', 'LotArea', 'YearBuilt', '1stFlrSF', 'FullBath', 'BedroomAbvGr',
-            'KitchenAbvGr', 'GarageArea']
-test_X = test_data[test_features]
-test_X = np.nan_to_num(test_X)
+
+test_X = test_data[features]
+test_X = test_X.fillna(train_X.mean())
 test_preds = rf_model.predict(test_X)
-
-# The lines below shows how to save predictions in format used for competition scoring
-# Just uncomment them.
-
 output = pd.DataFrame({'Id': test_data.Id,
                        'SalePrice': test_preds})
 output.to_csv('submission.csv', index=False)

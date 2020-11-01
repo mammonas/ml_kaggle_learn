e09d454af7db5cec14aacd3253a337a8d0b7ac86
Khanh <daugauvuontre@gmail.com>
2020-11-01 11:03:01 +0700

    21715

--- basic/home_pricing/home.py
+++ basic/home_pricing/home.py
@@ -2,18 +2,25 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 train_file_path = './home-data-for-ml-course/train.csv'
 train_data = pd.read_csv(train_file_path)
-features = ['MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', '1stFlrSF', 'FullBath', 'BedroomAbvGr',
+colsOriginal = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', '1stFlrSF', 'FullBath', 'BedroomAbvGr',
             'KitchenAbvGr', 'GarageArea']
-
+colsToTransfer = ['MSZoning', 'Street', 'LotShape', 'LandContour', 'Utilities', 'Neighborhood']
+features = colsOriginal + colsToTransfer
 train_X = train_data[features]
-train_X['MSZoning'].loc[:] = LabelEncoder().fit_transform(train_X['MSZoning'].loc[:].values)
+for key in colsToTransfer:
+    default_key = 'RL' if key == 'MSZoning' else 'NA'
+    train_X[key] = train_X[key].fillna(default_key)
+
 train_X = train_X.fillna(train_X.mean())
+columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), colsToTransfer)], remainder='passthrough')
+train_X = columnTransformer.fit_transform(train_X)
 train_y = train_data.SalePrice
 
 s_train_X, s_val_X, s_train_y, s_val_y = train_test_split(train_X, train_y, random_state=0)
@@ -32,10 +39,11 @@ print('MAE %i' % mae)
 # test_data = pd.read_csv(test_file_path)
 #
 # test_X = test_data[features]
-# test_X['MSZoning'] = test_X['MSZoning'].fillna('X')
-# test_X['MSZoning'].loc[:] = LabelEncoder().fit_transform(test_X['MSZoning'].loc[:].values)
-#
-# test_X = test_X.fillna(train_X.mean())
+# for key in colsToTransfer:
+#     default_key = 'RL' if key == 'MSZoning' else 'NA'
+#     test_X[key] = test_X[key].fillna(default_key)
+# test_X = test_X.fillna(test_X.mean())
+# test_X = columnTransformer.fit_transform(test_X)
 # test_preds = rf_model.predict(test_X)
 # output = pd.DataFrame({'Id': test_data.Id,
 #                        'SalePrice': test_preds})

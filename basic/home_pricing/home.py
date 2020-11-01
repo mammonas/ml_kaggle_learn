d6095c6bddeb10540c28ec4d552dab41d1b45623
Khanh <daugauvuontre@gmail.com>
2020-11-01 10:00:26 +0700

    22038

--- basic/home_pricing/home.py
+++ basic/home_pricing/home.py
@@ -2,15 +2,17 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import LabelEncoder
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 train_file_path = './home-data-for-ml-course/train.csv'
 train_data = pd.read_csv(train_file_path)
-features = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', '1stFlrSF', 'FullBath', 'BedroomAbvGr',
+features = ['MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', '1stFlrSF', 'FullBath', 'BedroomAbvGr',
             'KitchenAbvGr', 'GarageArea']
 
 train_X = train_data[features]
+train_X['MSZoning'].loc[:] = LabelEncoder().fit_transform(train_X['MSZoning'].loc[:].values)
 train_X = train_X.fillna(train_X.mean())
 train_y = train_data.SalePrice
 
@@ -30,6 +32,9 @@ print('MAE %i' % mae)
 # test_data = pd.read_csv(test_file_path)
 #
 # test_X = test_data[features]
+# test_X['MSZoning'] = test_X['MSZoning'].fillna('X')
+# test_X['MSZoning'].loc[:] = LabelEncoder().fit_transform(test_X['MSZoning'].loc[:].values)
+#
 # test_X = test_X.fillna(train_X.mean())
 # test_preds = rf_model.predict(test_X)
 # output = pd.DataFrame({'Id': test_data.Id,

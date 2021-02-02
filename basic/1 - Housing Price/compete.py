3d3ebb0d46bb967c4808665ec6262511bf342300
Khanh <daugauvuontre@gmail.com>
2021-02-02 22:31:02 +0700

    Still need dealing with bugs

--- basic/1 - Housing Price/compete.py
+++ basic/1 - Housing Price/compete.py
@@ -3,27 +3,28 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 
 
-def score_model(model, X_t, X_v, y_t, y_v):
-    model.fit(X_t, y_t)
-    predictions = model.predict(X_v)
+def score_model(md, X_t, X_v, y_t, y_v):
+    md.fit(X_t, y_t)
+    predictions = md.predict(X_v)
     return mean_absolute_error(y_v, predictions)
 
 
-def get_best_model(models, X_t, X_v, y_t, y_v):
+def get_best_model(mds, X_t, X_v, y_t, y_v):
     best_model = None
     lowest_mea = None
-    for i in range(0, len(models)):
-        mea = score_model(models[i], X_t, X_v, y_t, y_v)
+    for i in range(0, len(mds)):
+        mea = score_model(mds[i], X_t, X_v, y_t, y_v)
         print("MEA is %d" % mea)
         if i == 0:
-            best_model = models[i]
+            best_model = mds[i]
             lowest_mea = mea
         else:
             if lowest_mea > mea:
                 lowest_mea = mea
-                best_model = models[i]
+                best_model = mds[i]
     print("Best MEA is %d of model %s" % (lowest_mea, best_model))
     return best_model
 
@@ -33,15 +34,27 @@ train_data = pd.read_csv(train_file_path)
 # print(train_data.head())
 
 y = train_data.SalePrice
-features = train_data.select_dtypes(include='number').columns.drop(['SalePrice'])
-X = train_data[features]
-# print(X.head())
+X = train_data[train_data.columns.drop(['SalePrice'])]
+
 
 X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
 missing_values_cols = [col for col in X_train.columns if X_train[col].isnull().any()]
 X_train = X_train.drop(missing_values_cols, axis=1)
 X_val = X_val.drop(missing_values_cols, axis=1)
-# print(X_train.head())
+
+categorical_features = train_data.select_dtypes(exclude='number').columns
+oh_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+X_train_oh = pd.DataFrame(oh_encoder.fit_transform(X_train[categorical_features]))
+X_val_oh = pd.DataFrame(oh_encoder.transform(X_val[categorical_features]))
+
+X_train_oh.index = X_train.index
+X_val_oh.index = X_val.index
+
+X_train_num = X_train.drop(categorical_features, axis=1).drop(['SalePrice'])
+X_val_num = X_val.drop(categorical_features, axis=1).drop(['SalePrice'])
+
+X_train_final = pd.concat([X_train_oh, X_train_num], axis=1)
+X_val_final = pd.concat([X_val_oh, X_val_num], axis=1)
 
 models = [
     RandomForestRegressor(n_estimators=50, random_state=0),
@@ -51,24 +64,24 @@ models = [
     RandomForestRegressor(n_estimators=100, max_depth=7, random_state=0)
 ]
 
-model = get_best_model(models, X_train, X_val, y_train, y_val)
+model = get_best_model(models, X_train_final, X_val_final, y_train, y_val)
 
-X_train_full = X.drop(missing_values_cols, axis=1)
-model.fit(X_train_full, y)
-
-test_file_path = 'home-data-for-ml-course/test.csv'
-test_data = pd.read_csv(test_file_path)
-
-X_test = test_data[X_train.columns]
-X_test = X_test.fillna(X_test.mean())
-
-preds = model.predict(X_test)
-print(len(preds))
-output = pd.DataFrame({
-    'Id': X_test.Id,
-    'SalePrice': preds
-})
-output.to_csv('submission_17756.csv', index=False)
+# X_train_full = X.drop(missing_values_cols, axis=1)
+# model.fit(X_train_full, y)
+#
+# test_file_path = 'home-data-for-ml-course/test.csv'
+# test_data = pd.read_csv(test_file_path)
+#
+# X_test = test_data[X_train.columns]
+# X_test = X_test.fillna(X_test.mean())
+#
+# preds = model.predict(X_test)
+# print(len(preds))
+# output = pd.DataFrame({
+#     'Id': X_test.Id,
+#     'SalePrice': preds
+# })
+# output.to_csv('submission_17756.csv', index=False)
 
 # https://www.kaggle.com/mammonas/exercise-introduction/edit
 # https://www.kaggle.com/alexisbcook/missing-values
